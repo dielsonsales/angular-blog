e333ab0f8f1f425176e36ccce138569605072f1c
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2025-08-23 14:04:47 -0300

    Create initial content on header and footer components

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
-export class Header {
-
-}
+export class Header { }
